c6f057fc570fcec7e1be6bdce9fef83b8a3771f9
NikitaSPBU <st111245@student.spbu.ru>
2023-02-09 15:14:58 +0300

    add some comments, Dump file

--- MoleculesInteraction.py
+++ MoleculesInteraction.py
@@ -4,19 +4,26 @@ import os
 
 
 def start(**args):
+    """ Считывание данных из кортежа """
     AgRadius, AgMass, AlNumOfAtoms, AlRadius, AlMass = \
         args['AgRadius'], args['AgMass'], args['AlNumOfAtoms'], args['AlRadius'], args['AlMass']
     TimeStep, Steps, OutputFrequency, Borders, OutputFileName = \
         args['TimeStep'], args['Steps'], args['OutputFrequency'], args['Borders'], args['OutputFileName']
 
-    dimension = len(Borders)
+    dimension = len(Borders) # Вычислние размерности
+
+    """ Преобразование значений в массивы для удобного использования """
 
     AlMass = np.ones(AlNumOfAtoms) * AlMass
     AlRadius = np.ones(AlNumOfAtoms) * AlRadius
-    AlVelocity = np.array([[0 for i in range(dimension)] for j in range(AlNumOfAtoms)])
-    AlPositions = np.array([[0 for i in range(dimension)] for j in range(AlNumOfAtoms)])
+    AlVelocity = np.array([[0.0 for i in range(dimension)] for j in range(AlNumOfAtoms)])
+    AlPositions = np.array([[0.0 for i in range(dimension)] for j in range(AlNumOfAtoms)])
 
-    for i in range(dimension):
+    coeff = 1/AlNumOfAtoms
+    for i in range(AlNumOfAtoms):
+        AlPositions[i][0], AlPositions[i][1] = coeff*i, 1/2 + 1/2 * i%2
+
+    for i in range(dimension): # Подгонка значений относительно границ области
         AlPositions[:, i] = Borders[i][0] + (Borders[i][1] - Borders[i][0]) * AlPositions[:, i]
 
     if os.path.exists(OutputFileName):
@@ -24,4 +31,5 @@ def start(**args):
 
     step = 0
 
-    Dump.writeOutput(OutputFileName, AlNumOfAtoms, step, Borders, radius=AlRadius, pos=AlPositions, v=AlVelocity)
+    Dump.writeOutput(OutputFileName, AlNumOfAtoms, step, Borders,
+                     radius=AlRadius, pos=AlPositions, v=AlVelocity) # Вывод данных в .dump файл
